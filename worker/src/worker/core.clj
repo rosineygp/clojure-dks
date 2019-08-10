1c9a622c7faeafd77e7e75147c3dcc1b6483c310
Rosiney Gomes Pereira <rosiney.gp@gmail.com>
2019-08-10 20:07:23 +0000

    - create cni for docker - add tests - update pipeline, include docker for worker test

--- worker/src/worker/core.clj
+++ worker/src/worker/core.clj
@@ -1,7 +1,15 @@
 (ns worker.core
+  (:require [worker.cni :refer :all :as cni])
   (:gen-class))
+
+(use '[clojure.java.shell :only [sh]])
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
+  (println "---")
+  (println  (cni/docker-run "ubuntu" ["ls" "-la"]))
+  (println  (cni/docker-run "ubuntu" ["ps" "xua"]))
+  (println  (cni/docker-run "ubuntu" ["000000a11"]))
+  (println "---")
   (println "Hello, World!"))
--- worker/test/worker/core_test.clj
+++ worker/test/worker/core_test.clj
@@ -1,7 +1,12 @@
 (ns worker.core-test
   (:require [clojure.test :refer :all]
-            [worker.core :refer :all]))
+            [worker.core :refer :all]
+            [worker.cni :refer :all :as cni]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 1 1))))
+; (deftest a-test
+;   (testing "FIXME, I fail."
+;     (is (= 1 1))))
+
+; (deftest cni
+;   (testing "docker run"
+;     (is (= (re-matches #"[A-Za-z0-9]{12}" (cni/docker-run "alpine" "ls -la /"))))))
