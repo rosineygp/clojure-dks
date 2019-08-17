a101d2016ccdc7af9d10634073113ccdb065fb4a
Rosiney Gomes Pereira <rosiney.gp@gmail.com>
2019-08-17 02:40:01 +0000

    Added linter for worker
    - linter eastwood
    - Find some bugs, already fixed
    
    ! linter is awesome

--- worker/project.clj
+++ worker/project.clj
@@ -9,4 +9,5 @@
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
              :dev {:plugins [[lein-cljfmt "0.6.4"]
-                             [lein-cloverage "1.1.1"]]}})
+                             [lein-cloverage "1.1.1"]
+                             [jonase/eastwood "0.3.5"]]}})
